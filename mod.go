0c48973541ee82cb096a23e38ece722d5e4709cd
Hank000AAA <154630877@qq.com>
2020-03-21 09:51:17 +0800

    233

--- mod.go
+++ mod.go
@@ -5,13 +5,7 @@ import (
 	"errors"
 )
 
-func Hi(name string, lang string) (string, error) {
-	switch(lang) {
-	case "en":
-		return fmt.Sprintf("Hi, %s!", name), nil
-	case "ch":
-		return fmt.Sprintf("你好, %s!", name), nil
-	}
-	return "", errors.New("unknow language!")
+func Hi(name string, lang string) (string) {
+	fmt.Sprintf("%s Hi 233!", name)
 }
 
